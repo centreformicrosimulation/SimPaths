5acd502ab7ce759449fb534c52e48bbd0e4c72c7
pbronka <56582427+pbronka@users.noreply.github.com>
2025-03-25 06:50:05 +0000

    Update ActivityAlignmentMacroShock.java

--- src/main/java/simpaths/model/ActivityAlignmentMacroShock.java
+++ src/main/java/simpaths/model/ActivityAlignmentMacroShock.java
@@ -5,26 +5,28 @@ import microsim.engine.SimulationEngine;
 import simpaths.data.IEvaluation;
 import simpaths.data.Parameters;
 import simpaths.model.enums.Occupancy;
+import simpaths.model.enums.OccupancyMacroShock;
 import simpaths.model.enums.TargetShares;
 import simpaths.model.enums.Les_c4;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.List;
 
 public class ActivityAlignmentMacroShock implements IEvaluation {
 
     private double targetAggregateShareOfEmployedPersons;
     private double utilityAdjustment;
     boolean utilityAdjustmentChanged;
-    Map<Occupancy, MultiKeyCoefficientMap> coefficientMaps;
-    Map<Occupancy, String> regressorsToModify;
+    Map<OccupancyMacroShock, MultiKeyCoefficientMap> coefficientMaps;
+    Map<OccupancyMacroShock, List<String>> regressorsToModify;
     private Set<Person> persons;
     private Set<BenefitUnit> benefitUnits;
     private SimPathsModel model;
 
     public ActivityAlignmentMacroShock(Set<Person> persons, Set<BenefitUnit> benefitUnits,
-                                       Map<Occupancy, MultiKeyCoefficientMap> coefficientMaps,
-                                       Map<Occupancy, String> regressorsToModify,
+                                       Map<OccupancyMacroShock, MultiKeyCoefficientMap> coefficientMaps,
+                                       Map<OccupancyMacroShock, List<String>> regressorsToModify,
                                        double utilityAdjustment) {
         this.model = (SimPathsModel) SimulationEngine.getInstance().getManager(SimPathsModel.class.getCanonicalName());
         this.persons = persons;
@@ -57,19 +59,21 @@ public class ActivityAlignmentMacroShock implements IEvaluation {
     }
 
     private void adjustEmployment(double newUtilityAdjustment) {
-        for (Map.Entry<Occupancy, MultiKeyCoefficientMap> entry : coefficientMaps.entrySet()) {
-            Occupancy occupancy = entry.getKey();
+        for (Map.Entry<OccupancyMacroShock, MultiKeyCoefficientMap> entry : coefficientMaps.entrySet()) {
+            OccupancyMacroShock occupancy = entry.getKey();
             MultiKeyCoefficientMap map = entry.getValue();
-            String regressorToModify = regressorsToModify.get(occupancy);
+            List<String> regressors = regressorsToModify.get(occupancy);
 
-            Object currentValueObj = map.getValue(regressorToModify);
-            if (!(currentValueObj instanceof Number)) {
-                throw new IllegalArgumentException("Expected a numeric value for key: " + regressorToModify);
+            for (String regressor : regressors) {
+                Object currentValueObj = map.getValue(regressor);
+                if (!(currentValueObj instanceof Number)) {
+                    throw new IllegalArgumentException("Expected a numeric value for key: " + regressor);
+                }
+
+                double currentValue = ((Number) currentValueObj).doubleValue();
+                double newValue = currentValue + newUtilityAdjustment;
+                map.replaceValue(regressor, newValue);
             }
-
-            double currentValue = ((Number) currentValueObj).doubleValue();
-            double newValue = currentValue + newUtilityAdjustment;
-            map.replaceValue(regressorToModify, newValue);
         }
 
         benefitUnits.parallelStream()
