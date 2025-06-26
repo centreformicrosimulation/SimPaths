7c6a83669f3776afaa4040bc09474bc6e14a9a56
Bronka <pbronk@essex.ac.uk>
2025-06-26 09:01:44 +0100

    Update config files

--- src/main/java/simpaths/experiment/SimPathsMultiRun.java
+++ src/main/java/simpaths/experiment/SimPathsMultiRun.java
@@ -65,10 +65,10 @@ public class SimPathsMultiRun extends MultiRun {
 
 		// set default values for country and start year
 		MultiKeyCoefficientMap lastDatabaseCountryAndYear = ExcelAssistant.loadCoefficientMap("input" + File.separator + Parameters.DatabaseCountryYearFilename + ".xlsx", "Data", 1, 1);
-		if (lastDatabaseCountryAndYear.keySet().stream().anyMatch(key -> key.toString().equals("MultiKey[IT]"))) {
-			countryString = "Italy";
+		if (lastDatabaseCountryAndYear.keySet().stream().anyMatch(key -> key.toString().equals("MultiKey[EL]"))) {
+			countryString = "Greece";
 		} else {
-			countryString = "Italy";
+			countryString = "Greece";
 		}
 		country = Country.getCountryFromNameString(countryString);
 		String valueYear = lastDatabaseCountryAndYear.getValue(country.toString()).toString();
